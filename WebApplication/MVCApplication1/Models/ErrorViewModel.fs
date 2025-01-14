3e925a5ab52f894ec57b888a465aca6f3e7bb450
Atharvashirsh Tiwary <121449422+Atharvashirsh@users.noreply.github.com>
2025-01-14 11:05:36 +0530

    removed files

--- WebApplication/MVCApplication1/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace MVCApplication1.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
